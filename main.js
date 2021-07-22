ca727093ae04399cec8f14828bea554b93e8bd3d
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-07-22 16:37:54 +0530

    addin webacm preview and loading ml5

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 //This is main.js
+
+Webcam.set({
+    width: 340,
+    height: 300,
+    image_format: "png",
+    image_quality: 90
+});
+
+var camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='"+data_uri+"'>";
+    });
+}
+
+console.log("ml5 version: ",ml5.version);
+
+var classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/-57PUIvXf/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded!");
+};
